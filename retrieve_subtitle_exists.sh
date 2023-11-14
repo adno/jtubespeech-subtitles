3cc1193d99af975f33282b743dace7f478152324
Adam Nohejl <adam@nohejl.name>
2023-11-14 10:17:53 +0900

    Shell scripts (retrieve in parallel)

---
+++ retrieve_subtitle_exists.sh
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+# for lang in en ja ps # es fr pt zh it pl de ru cs
+# do
+# 	cat videoid/${lang}/${lang}wiki-latest-pages-articles-multistream-index_part*.txt \
+# 		| sort -u > videoid/${lang}/${lang}.txt
+# done
+
+# for lang in en ja ps es # fr pt zh it pl de ru cs
+# do
+# 	# 4k items can run in reasonable time: 4000 * 3 s = 3h 20min
+# 	split -l 4000 -d -a 6 videoid/${lang}/${lang}.txt videoid/${lang}/${lang}_part
+# done
+
+
+# (
+# for lang in ja en ps es
+# do
+# 	for file in videoid/${lang}/${lang}_part*
+# 	do
+# 		echo "${lang},${file}"
+# 	done
+# done
+# ) > videoid/tasks_jaenpses.csv
+
+# split -l 96 -d -a 6 videoid/tasks_jaenpses.csv videoid/tasks_jaenpses_part
+
+TASKNO="$1"
+if [ -z "$TASKNO" ]
+then
+	echo "Need TASKNO as argument." >&2
+	exit 1
+fi
+
+TASKNO="$(printf '%06d' $TASKNO)"
+TASKS="videoid/tasks_jaenpses_part$TASKNO"
+LOG="retrieve_${TASKNO}.log"
+ 
+parallel --joblog "$LOG" --colsep ',' python scripts/retrieve_subtitle_exists.py  < "$TASKS"
