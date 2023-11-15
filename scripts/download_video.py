a0ceefc98de23985db4045a6a3c7dca414c970a4
Adam Nohejl <adam@nohejl.name>
2023-11-15 16:08:30 +0900

    Use yt_dlp programmatically for downloading

--- scripts/download_video.py
+++ scripts/download_video.py
@@ -1,13 +1,14 @@
 import time
 import argparse
 import sys
-import subprocess
 import shutil
 # import pydub -- imported contitionally - not necessary for --subtitles-only
 from pathlib import Path
-from util import make_video_url, make_basename, vtt2txt, autovtt2txt
+from util import make_video_url, make_basename, vtt2txt  # UNUSED: autovtt2txt
 import pandas as pd
 from tqdm import tqdm
+from yt_dlp import YoutubeDL
+
 
 def parse_args():
   parser = argparse.ArgumentParser(
@@ -22,6 +23,7 @@ def parse_args():
   parser.add_argument("--subtitles-only", action='store_true', default=False, help="download subtitles, no audio")
   return parser.parse_args(sys.argv[1:])
 
+
 def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False, subs_only=False):
   """
   Tips:
@@ -34,63 +36,86 @@ def download_video(lang, fn_sub, outdir="video", wait_sec=10, keep_org=False, su
 
   sub = pd.read_csv(fn_sub)
 
-  for videoid in tqdm(sub[sub["sub"]==True]["videoid"]): # manual subtitle only
-    fn = {}
-    for k in (["vtt", "txt"] if subs_only else ["wav", "wav16k", "vtt", "txt"]):
-      fn[k] = Path(outdir) / lang / k / (make_basename(videoid) + "." + k[:3])
-      fn[k].parent.mkdir(parents=True, exist_ok=True)
+  dl_path_tmpl = str(
+    Path(outdir) / lang / ('txt' if subs_only else 'wav') / '%(id).2s' / '%(id)s.%(ext)s'
+    )
 
-    exists = (subs_only or fn["wav16k"].exists()) and fn["txt"].exists()
-    if not exists:
-      print(videoid)
+  ydl_opts = {
+   'extract_flat': 'discard_in_playlist',
+   'extractor_args': {'youtube': {'player_client': ['web']}},
+   'fragment_retries': 10,
+   'ignoreerrors': 'only_download',
+   # Subdirectories based on first 2 chars compatible with make_basename():
+   'outtmpl': {'default': dl_path_tmpl},
+   'postprocessors': [{'key': 'FFmpegConcat',
+                       'only_multi_video': True,
+                       'when': 'playlist'}],
+   'retries': 10,
+   'skip_download': subs_only,
+   'subtitleslangs': [lang],
+   'writesubtitles': True
+   }
 
-      # download
-      # TODO: Use "yt-dlp" like the current version, is it worth it?
-      url = make_video_url(videoid)
-      if subs_only:
-        base = fn["txt"].parent.joinpath(fn["txt"].stem)
-        cp = subprocess.run(f"yt-dlp --sub-lang {lang} --extractor-args youtube:player-client=web --skip-download --write-sub {url} -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
-      else:
-        base = fn["wav"].parent.joinpath(fn["wav"].stem)
-        cp = subprocess.run(f"yt-dlp --sub-lang {lang} --extract-audio --audio-format wav --write-sub {url} -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
-      if cp.returncode != 0:
-        print(f"Failed to download the video: url = {url}")
-        continue
-      try:
-        shutil.move(f"{base}.{lang}.vtt", fn["vtt"])
-      except Exception as e:
-        print(f"Failed to rename subtitle file. The download may have failed: url = {url}, filename = {base}.{lang}.vtt, error = {e}")
-        continue
+  with YoutubeDL(ydl_opts) as ydl:
+    for videoid in tqdm(sub[sub["sub"]]["videoid"]): # manual subtitle only
+      fn = {}
+      for k in (["vtt", "txt"] if subs_only else ["wav", "wav16k", "vtt", "txt"]):
+        fn[k] = Path(outdir) / lang / k / (make_basename(videoid) + "." + k[:3])
+        fn[k].parent.mkdir(parents=True, exist_ok=True)
 
-      # vtt -> txt (reformatting)
-      try:
-        txt = vtt2txt(open(fn["vtt"], "r").readlines())
-        with open(fn["txt"], "w") as f:
-          f.writelines([f"{t[0]:1.3f}\t{t[1]:1.3f}\t\"{t[2]}\"\n" for t in txt])
-      except Exception as e:
-        print(f"Falied to convert subtitle file to txt file: url = {url}, filename = {fn['vtt']}, error = {e}")
-        continue
+      exists = (subs_only or fn["wav16k"].exists()) and fn["txt"].exists()
+      if not exists:
+        print(videoid)
 
-
-      if not subs_only:
-        # wav -> wav16k (resampling to 16kHz, 1ch)
+        # download
+        # TODO: Use "yt-dlp" like the current version, is it worth it?
+        url = make_video_url(videoid)
+        if subs_only:
+          base = fn["txt"].parent.joinpath(fn["txt"].stem)
+          # cp = subprocess.run(f"yt-dlp --sub-lang {lang} --extractor-args youtube:player-client=web --skip-download --write-sub {url} -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
+        else:
+          base = fn["wav"].parent.joinpath(fn["wav"].stem)
+          # cp = subprocess.run(f"yt-dlp --sub-lang {lang} --extract-audio --audio-format wav --write-sub {url} -o {base}.\%\(ext\)s", shell=True,universal_newlines=True)
+        dl_returncode = ydl.download(url)
+        if dl_returncode != 0:
+          print(f"Failed to download the video: url = {url}")
+          continue
         try:
-          wav = pydub.AudioSegment.from_file(fn["wav"], format = "wav")
-          wav = pydub.effects.normalize(wav, 5.0).set_frame_rate(16000).set_channels(1)
-          wav.export(fn["wav16k"], format="wav", bitrate="16k")
+          shutil.move(f"{base}.{lang}.vtt", fn["vtt"])
         except Exception as e:
-          print(f"Failed to normalize or resample downloaded audio: url = {url}, filename = {fn['wav']}, error = {e}")
+          print(f"Failed to rename subtitle file. The download may have failed: url = {url}, filename = {base}.{lang}.vtt, error = {e}")
           continue
 
-        # remove original wav
-        if not keep_org:
-          fn["wav"].unlink()
+        # vtt -> txt (reformatting)
+        try:
+          txt = vtt2txt(open(fn["vtt"], "r").readlines())
+          with open(fn["txt"], "w") as f:
+            f.writelines([f"{t[0]:1.3f}\t{t[1]:1.3f}\t\"{t[2]}\"\n" for t in txt])
+        except Exception as e:
+          print(f"Failed to convert subtitle file to txt file: url = {url}, filename = {fn['vtt']}, error = {e}")
+          continue
 
-      # wait
-      if wait_sec > 0.01:
-        time.sleep(wait_sec)
+
+        if not subs_only:
+          # wav -> wav16k (resampling to 16kHz, 1ch)
+          try:
+            wav = pydub.AudioSegment.from_file(fn["wav"], format="wav")
+            wav = pydub.effects.normalize(wav, 5.0).set_frame_rate(16000).set_channels(1)
+            wav.export(fn["wav16k"], format="wav", bitrate="16k")
+          except Exception as e:
+            print(f"Failed to normalize or resample downloaded audio: url = {url}, filename = {fn['wav']}, error = {e}")
+            continue
+
+          # remove original wav
+          if not keep_org:
+            fn["wav"].unlink()
+
+        # wait
+        if wait_sec > 0.01:
+          time.sleep(wait_sec)
 
   return Path(outdir) / lang
+
 
 if __name__ == "__main__":
   args = parse_args()
@@ -103,4 +128,3 @@ if __name__ == "__main__":
     keep_org=args.keeporg, wait_sec=args.wait, subs_only=args.subtitles_only
     )
   print(f"save {args.lang.upper()} videos to {dirname}.")
-
