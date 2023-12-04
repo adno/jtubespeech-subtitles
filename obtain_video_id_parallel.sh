465f92d257383ef611de8d9935e55752695c81d6
Adam Nohejl <adam@nohejl.name>
2023-12-04 14:57:20 +0900

    Add Google options to obtain video

--- obtain_video_id_parallel.sh
+++ obtain_video_id_parallel.sh
@@ -1,5 +1,15 @@
 #!/bin/bash
 
+OPT="$1"
+if [ "$OPT" == '--google' ]
+then
+	OPTIONS='--google --outdir videoid-google'
+	shift
+else
+	OPTIONS=''
+fi
+
+
 TASKS="$1"
 if [ -z "$TASKS" ]
 then
@@ -12,4 +22,4 @@ then
 	LOG="obtain_vid.log"
 fi
 
-parallel --gnu --joblog "$LOG" --colsep ',' python scripts/obtain_video_id.py < "$TASKS"
+parallel --gnu --joblog "$LOG" --colsep ',' python scripts/obtain_video_id.py $OPTIONS < "$TASKS"
--- scripts/obtain_video_id.py
+++ scripts/obtain_video_id.py
@@ -1,12 +1,13 @@
 import time
 import requests
 import argparse
-import re
 import sys
 from pathlib import Path
-from util import make_query_url
+from util import make_query_url, html2videoids
 from tqdm import tqdm
 
+# https://www.google.com/search?q=hippo&tbm=vid&tbs=cc:1,srcf:H4sIAAAAAAAAAC3LMQ6AIAwF0NuwGDt4o0IgqaZ8gm0It1eJ4xveNuHmMVOCBkWFcceC1ALlbi_1obCEKGvfrF8R4d-L0YRSdNY_17WO8B3F8UT1IAAAA&num=100
+# ids = re.findall(r'/url\?q=https://www\.youtube\.com/watch%3Fv%3D([^&]+)&amp;', s)
 
 def parse_args():
   parser = argparse.ArgumentParser(
@@ -17,11 +18,14 @@ def parse_args():
   parser.add_argument("wordlist", type=str, help="filename of word list")
   parser.add_argument("--outdir", type=str, default="videoid", help="dirname to save video IDs")
   parser.add_argument("--wait",   type=float, default=0.0, help="seconds to wait between words (default: 0.0)")
-  parser.add_argument("--keep-alive", action=argparse.BooleanOptionalAction, default=True)
+  parser.add_argument("--keep-alive",       action=argparse.BooleanOptionalAction, default=True, help='HTTP keep-alive')
+  parser.add_argument("--google",           action=argparse.BooleanOptionalAction, default=False, help='Search using Google instead of YouTube')
+  parser.add_argument("--google-max-pages", type=int, default=2, help='Max search result pages (100 results) retrieved from Google per query')
   return parser.parse_args(sys.argv[1:])
 
 
-def obtain_video_id(lang, fn_word, outdir="videoid", wait_sec=0.2, keep_alive=True):
+def obtain_video_id(lang, fn_word, outdir="videoid", wait_sec=0.2,
+                    keep_alive=True, google=False, max_pages=2):
   fn_videoid = Path(outdir) / lang / f"{Path(fn_word).stem}.txt"
   fn_videoid.parent.mkdir(parents=True, exist_ok=True)
 
@@ -30,19 +34,24 @@ def obtain_video_id(lang, fn_word, outdir="videoid", wait_sec=0.2, keep_alive=Tr
   with open(fn_videoid, "w") as f:
     for word in tqdm(list(open(fn_word, "r").readlines())):
       try:
-        # download search results
-        url = make_query_url(word)
-        html = requests.get(url).content
 
-        # find video IDs
-        videoids_found = [x.split(":")[1].strip("\"").strip(" ") for x in re.findall(r"\"videoId\":\"[\w\_\-]+?\"", str(html))]
-        videoids_found = list(set(videoids_found))
+        videoids_found = set()
+        for i in range(max_pages if google else 1):
+            # download search results
+            url = make_query_url(word, google=google, index=i)
+            html = get_url(url).content
+            # find video IDs
+            page_videoids = html2videoids(html, google=google)
+            if not page_videoids:
+                break
+            videoids_found.update(page_videoids)
 
         # write
         f.writelines([v + "\n" for v in videoids_found])
         f.flush()
-      except:
-        print(f"No video found for {word}.")
+      except Exception as e:
+        # TODO: not found causes an exception?
+        print(f"No video found for {word}, error = {e}.")
 
       # wait
       if wait_sec > 0.01:
@@ -55,5 +64,7 @@ if __name__ == "__main__":
   args = parse_args()
 
   filename = obtain_video_id(args.lang, args.wordlist, args.outdir,
-    wait_sec=args.wait, keep_alive=args.keep_alive)
+    wait_sec=args.wait, keep_alive=args.keep_alive,
+    google=args.google, max_pages=args.google_max_pages
+    )
   print(f"save {args.lang.upper()} video IDs to {filename}.")
